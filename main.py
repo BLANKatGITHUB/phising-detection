e8120029972ec4dfe4e9028087ba7a957442e8ac
aaishwarymishra <aaishwarymishra@gmail.com>
2025-03-09 09:27:43 +0000

    Added url input page

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_file('src/index.html')
+    return render_template('index.html')
+
+@app.post("/url")
+def prediction():
+    url = request.form["url"]
+    return f"url recevived {url}"
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
